a6cf61f93538fec28f238fdc2bac69335c0ed240
orientman <orientman@gmail.com>
2015-06-19 13:47:08 +0200

    Monada ROP

---
+++ FSharp/Rop.fs
@@ -0,0 +1,22 @@
+﻿// Railway Oriented Programming
+// See: http://fsharpforfunandprofit.com/posts/recipe-part2/
+module Rop
+
+type Result<'TSuccess, 'TFailure> =
+    | Success of 'TSuccess
+    | Failure of 'TFailure
+
+let bind switchFunction twoTrackInput = 
+    match twoTrackInput with
+    | Success s -> switchFunction s
+    | Failure f -> Failure f
+
+let map oneTrackFunction twoTrackInput = 
+    match twoTrackInput with
+    | Success s -> Success (oneTrackFunction s)
+    | Failure f -> Failure f
+
+let (>>=) twoTrackInput switchFunction = 
+    bind switchFunction twoTrackInput 
+
+let (<!>) m f = map f m
---
+++ FSharp/Rop.fsx
@@ -0,0 +1,18 @@
+﻿#load "Rop.fs"
+
+open Rop
+
+type ErrorCode = DivideByZero | InvalidInt of string
+
+let safeDiv a b =
+    if b = 0 then Failure(DivideByZero) else Success(a / b)
+
+let tryParse value =
+    match System.Int32.TryParse value with
+    | (true, result) -> Success(result)
+    | _ -> Failure(InvalidInt value)
+
+tryParse "10" <!> (*) 2 >>= safeDiv 100
+//tryParse "10" |> map ((*) 2) >>= safeDiv 100
+//tryParse "10" |> map ((*) 2) |> bind (safeDiv 100)
+tryParse "0x" <!> (*) 2 >>= safeDiv 100
